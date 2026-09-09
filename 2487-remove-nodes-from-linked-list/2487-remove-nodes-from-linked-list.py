6d2d62032a821fc1d748c15791e7959f13578712
Tarun p <tarun654p@gmail.com>
2026-09-09 23:55:50 +0530

    Time: 114 ms (57.71%), Space: 63.2 MB (61.43%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -5,27 +5,32 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        def reverse(node):
+        # head = [5, 2, 13, 3, 8]
+        #         ^
+        #                ^
+        # 5 -> 2 -> 13 -> 3 -> 8
+        # 3 <- 2 <- 13 <- 3 <- 8
+
+        def reverse(nodeHead):
             prev = None
-            while node:
-                nxt = node.next
-                node.next = prev
-                prev = node
-                node = nxt
+            curr = nodeHead
+            while curr:
+                nextNode = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nextNode
             return prev
         
-        head = reverse(head)
+        revHead = reverse(head)
+        curr = revHead
+        maxSeen = curr.val
 
-        max_val = head.val
-        curr = head
-        
-        while curr and curr.next:
-            if curr.next.val < max_val:
+        while curr.next:
+            if curr.next.val < maxSeen:
                 curr.next = curr.next.next
             else:
+                maxSeen = curr.next.val
                 curr = curr.next
-                max_val = curr.val
+
         
-        return reverse(head)
-        
+        return reverse(revHead)
