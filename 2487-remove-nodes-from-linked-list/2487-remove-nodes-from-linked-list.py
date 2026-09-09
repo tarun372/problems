bcc02823ed1e62f8b23bc795d09ab7bd6a3b798b
Tarun p <tarun654p@gmail.com>
2026-09-10 00:02:39 +0530

    Time: 121 ms (49.43%), Space: 63.5 MB (56.48%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -1,17 +1,24 @@
 class Solution:
-    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur = head
+    def removeNodes(self, head):
         stack = []
-        while cur:
-            while stack and stack[-1].val < cur.val:
+        curr = head
+        
+        # Phase 1: Filter out the smaller nodes using the stack
+        while curr:
+            # If the new node is BIGGER than the top of our stack, rip up the top note!
+            while stack and stack[-1].val < curr.val:
                 stack.pop()
-            stack.append(cur)
-            cur = cur.next
+            
+            # Put the current node on the stack
+            stack.append(curr)
+            curr = curr.next
+            
+        # Phase 2: Link the surviving nodes back together
+        for i in range(len(stack) - 1):
+            stack[i].next = stack[i+1]
+            
+        # Make sure the very last node points to nothing
+        stack[-1].next = None
         
-        nxt = None
-        while stack:
-            cur = stack.pop()
-            cur.next = nxt
-            nxt = cur
-        
-        return cur
+        # The bottom of the stack is the new head of our linked list
+        return stack[0]
