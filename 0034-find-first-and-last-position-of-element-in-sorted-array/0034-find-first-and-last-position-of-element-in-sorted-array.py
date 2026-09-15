a08e4e8cc465d742ea7a9c3660dc33c5157109e8
Tarun p <tarun654p@gmail.com>
2026-09-16 01:09:46 +0530

    Time: 0 ms (100%), Space: 20.4 MB (91.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,36 +1,35 @@
 class Solution(object):
     def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        def first():
-            ans=-1
-            low=0
-            high=len(nums)-1
-            while(low<=high):
-                mid=(low+high)//2
-                if(target==nums[mid]):
-                    ans=mid
-                    high=mid-1
-                elif(nums[mid]>target):
-                    high=mid-1
-                elif(nums[mid]<target):
-                    low=mid+1
+        def find_bound(is_first):
+            ans = -1
+            low = 0
+            high = len(nums) - 1
+            
+            while low <= high:
+                # Good habit: prevents integer overflow in other languages
+                mid = low + (high - low) // 2 
+                
+                if nums[mid] == target:
+                    ans = mid
+                    if is_first:
+                        high = mid - 1  # Keep looking left
+                    else:
+                        low = mid + 1   # Keep looking right
+                        
+                elif nums[mid] > target:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+                    
             return ans
-        def last():
-            ans=-1
-            low=0
-            high=len(nums)-1
-            while(low<=high):
-                mid=(low+high)//2
-                if(target==nums[mid]):
-                    ans=mid
-                    low=mid+1
-                elif(target>nums[mid]):
-                    low=mid+1
-                elif(target<nums[mid]):
-                    high=mid-1
-            return ans
-        return [first(),last()]
+
+        # Call the same function, just flip the flag
+        first_pos = find_bound(is_first=True)
+        
+        # Early exit: if the first position isn't found, the target isn't in the array
+        if first_pos == -1:
+            return [-1, -1]
+            
+        last_pos = find_bound(is_first=False)
+        
+        return [first_pos, last_pos]
