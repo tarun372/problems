f5a085384272081ee2e8c5e7d8139d8ce2443a40
Tarun p <tarun654p@gmail.com>
2026-09-16 10:56:49 +0530

    Time: 0 ms (100%), Space: 31.4 MB (48.62%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,7 +1,21 @@
 class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        for i in range(2,len(arr)):
-            if arr[i]<arr[i-1] and arr[i-1]>arr[i-2]:
-                return i-1
-                break
+    def peakIndexInMountainArray(self, arr: list[int]) -> int:
+        start = 0
+        end = len(arr) - 1
         
+        # Notice we use `start < end` instead of `start <= end`
+        while start < end:
+            mid = start + (end - start) // 2
+            
+            # If the next element is greater, we are climbing up the mountain.
+            # The peak must be to the right of mid.
+            if arr[mid] < arr[mid + 1]:
+                start = mid + 1
+                
+            # If the next element is smaller, we are going down the mountain.
+            # The peak is either exactly at mid, or to the left of mid.
+            else:
+                end = mid
+                
+        # start and end will converge on the exact index of the peak
+        return start
