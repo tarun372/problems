ee57ebc11dbf7a1157af2d2c278f1ad59571bbb5
Tarun p <tarun654p@gmail.com>
2026-09-17 05:22:30 +0530

    Time: 135 ms (97.87%), Space: 30.2 MB (54.91%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -1,17 +1,19 @@
 class Solution:
-    def maximumCandies(self, candies: List[int], k: int) -> int:        
-        left, right = 1, max(candies)
-        result = 0
-        
-        while left <= right:
-            mid = (left + right) // 2
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        def isValidSize(group_candies):
+            groups = 0
+            for candy in candies:
+                groups += candy // group_candies # add groups that can be made from candy
+            return groups >= k
+
+        left, right = 0, sum(candies) // k # min is 0 candy per group, max is split all candies evenly
+
+        while left < right: # binary search to find max number of candies per group
+            mid = 1 + (left + right) // 2 # 1 + or ceil( / 2) to avoid looping with left = mid
             
-            children_count = sum(pile // mid for pile in candies)
-            
-            if children_count >= k:
-                result = mid
-                left = mid + 1
+            if isValidSize(mid): # true if can make k groups of mid size
+                left = mid # valid group, move left up and check higher group sizes
             else:
-                right = mid - 1
-                
-        return result
+                right = mid - 1 # invalid group, move right down and check lower group sizes
+        
+        return left # left = right = max valid size. right + 1 is first invalid size
