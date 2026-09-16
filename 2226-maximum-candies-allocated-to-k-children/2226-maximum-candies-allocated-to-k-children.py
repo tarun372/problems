f64ba0b5818fda90672e16dbe985aab829bddb80
Tarun p <tarun654p@gmail.com>
2026-09-17 05:21:46 +0530

    Time: 435 ms (29.63%), Space: 29.9 MB (96.02%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -1,21 +1,17 @@
-from typing import List
-
 class Solution:
-    def maximumCandies(self, candies: List[int], k: int) -> int:
-        if sum(candies) < k: return 0  # Not enough candies
-        
+    def maximumCandies(self, candies: List[int], k: int) -> int:        
         left, right = 1, max(candies)
         result = 0
         
         while left <= right:
             mid = (left + right) // 2
-            if self.canDistribute(candies, k, mid):
+            
+            children_count = sum(pile // mid for pile in candies)
+            
+            if children_count >= k:
                 result = mid
-                left = mid + 1  # Try larger `mid`
+                left = mid + 1
             else:
-                right = mid - 1  # Try smaller `mid`
-        
+                right = mid - 1
+                
         return result
-
-    def canDistribute(self, candies: List[int], k: int, val: int) -> bool:
-        return sum(c // val for c in candies) >= k
