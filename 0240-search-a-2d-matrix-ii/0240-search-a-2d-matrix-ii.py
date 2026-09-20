3330488fc4109c5313959f8fb970420d079206a0
Tarun p <tarun654p@gmail.com>
2026-09-20 21:32:58 +0530

    Time: 137 ms (78.65%), Space: 25.7 MB (23.82%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,28 +1,14 @@
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        if not matrix or not matrix[0]:
-            return False
-            
-        # Start at the top-right corner
-        row = 0
-        col = len(matrix[0]) - 1
+        row, col = 0, len(matrix[0]) - 1
         
-        # Keep searching as long as we haven't fallen off the grid
         while row < len(matrix) and col >= 0:
-            current = matrix[row][col]
-            
-            if current == target:
+            # Look up the value directly from the grid every time
+            if matrix[row][col] == target:
                 return True
-                
-            elif current > target:
-                # The current number is too big. 
-                # Since everything below it is even bigger, we can eliminate this entire column.
+            elif matrix[row][col] > target:
                 col -= 1
-                
             else:
-                # The current number is too small.
-                # Since everything to the left is even smaller, we can eliminate this entire row.
                 row += 1
                 
-        # We fell off the grid without finding the target
         return False
