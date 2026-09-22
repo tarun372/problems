6760cb9a9f36f748b0dc4cdade368ad326663ac0
Tarun p <tarun654p@gmail.com>
2026-09-22 23:36:17 +0530

    Time: 2 ms (92.21%), Space: 23.1 MB (32.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,24 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        # 1. Count the frequencies of each number
-        count = {}
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-            
-        # 2. Create buckets where the index is the frequency.
-        # The maximum possible frequency is the length of the array itself.
-        buckets = [[] for _ in range(len(nums) + 1)]
-        
-        # Put the numbers into their respective frequency buckets
+        count = Counter(nums)
+        heap = []
+
         for num, freq in count.items():
-            buckets[freq].append(num)
-            
-        # 3. Walk backwards from the highest possible frequency to gather the top K
-        res = []
-        for i in range(len(buckets) - 1, 0, -1):
-            for num in buckets[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
-                    
-        return res
+            heapq.heappush(heap, (freq, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return [num for _, num in heap]
